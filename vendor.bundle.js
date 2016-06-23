c51c0b76f13e060b03f7e24d9d8171efd0644d66
logicmyth <staley77@protonmail.com>
2016-06-22 21:39:56 -0400

    Grunt Build
    Ran Grunt Build to deploy

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(18);
+	module.exports = __webpack_require__(21);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 18:
+/***/ 21:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
